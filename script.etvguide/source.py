9f665b9f1b2b2fce013a4c3d1952eb0b2b6c9cd6
Pawel Kilian <pawel@yesware.com>
2015-10-13 11:04:02 -0400

    fixes

--- script.etvguide/source.py
+++ script.etvguide/source.py
@@ -879,12 +879,12 @@ class MTVGUIDESource(Source):
         if self.MTVGUIDEUrl == "Default":
             self.MTVGUIDEUrl = "https://www.dropbox.com/sh/77ypgoym2o8zoay/AACWgyV2Yd-btsWvc8j3o1Hba/epg.xml?dl=1"
         elif self.MTVGUIDEUrl == "Alternative":
-            self.MTVGUIDEUrl = "https://www.dropbox.com/sh/77ypgoym2o8zoay/AACWgyV2Yd-btsWvc8j3o1Hba/epg.xml?dl=1"
+            self.MTVGUIDEUrl = "http://xbsd.pl/~p/epg.xml"
         else:
             self.MTVGUIDEUrl = "https://www.dropbox.com/sh/77ypgoym2o8zoay/AACWgyV2Yd-btsWvc8j3o1Hba/epg.xml?dl=1"
 
         if ADDON.getSetting('epgzip').lower() == "true":
-            self.MTVGUIDEUrl = "https://www.dropbox.com/sh/77ypgoym2o8zoay/AACWgyV2Yd-btsWvc8j3o1Hba/epg.xml?dl=1"
+            self.MTVGUIDEUrl = "http://xbsd.pl/~p/epg.xml.zip"
 
     def getDataFromExternal(self, date, progress_callback = None):
         try:
