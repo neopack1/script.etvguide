e4bfea6ee24f745e9dfe7097b9158aea5efdd246
en0x <pawel.kilian@slalom.com>
2016-09-02 14:21:18 -0500

    tweaks

--- script.etvguide/serviceLib.py
+++ script.etvguide/serviceLib.py
@@ -96,7 +96,7 @@ class ShowList:
             return None
         return result_json
 
-    def getJsonFromExtendedAPI(self, url, post_data = None, save_cookie = False, load_cookie = False, cookieFile = None, jsonLoadsResult = False, jsonLoadResult = False, customHeaders = None):
+    def getJsonFromExtendedAPI(self, url, post_data = None, save_cookie = False, load_cookie = False, cookieFile = None, jsonLoadsResult = False, jsonLoadResult = False, customHeaders = None, max_conn_time = MAX_CONNECTION_TIME):
         result_json = None
         raw_json = None
         customOpeners = []
@@ -129,7 +129,7 @@ class ShowList:
             reqUrl = urllib2.Request(url=url, data=data, headers=headers)
 
             startTime = datetime.datetime.now()
-            while (datetime.datetime.now() - startTime).seconds < MAX_CONNECTION_TIME and strings2.M_TVGUIDE_CLOSING == False:
+            while (datetime.datetime.now() - startTime).seconds < max_conn_time and strings2.M_TVGUIDE_CLOSING == False:
                 try:
                     raw_json = urlOpen(reqUrl, customOpeners)
                     result_json = raw_json.read()
@@ -197,6 +197,111 @@ class ShowList:
             fileContent = None
 
         return fileContent
+
+    def parseDOM(self, html, name=u"", attrs={}, ret=False):
+        # Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
+
+        if isinstance(html, str):
+            try:
+                html = [html.decode("utf-8")] # Replace with chardet thingy
+            except:
+                html = [html]
+        elif isinstance(html, unicode):
+            html = [html]
+        elif not isinstance(html, list):
+            return u""
+
+        if not name.strip():
+            return u""
+
+        ret_lst = []
+        for item in html:
+            temp_item = re.compile('(<[^>]*?\n[^>]*?>)').findall(item)
+            for match in temp_item:
+                item = item.replace(match, match.replace("\n", " "))
+
+            lst = []
+            for key in attrs:
+                lst2 = re.compile('(<' + name + '[^>]*?(?:' + key + '=[\'"]' + attrs[key] + '[\'"].*?>))', re.M | re.S).findall(item)
+                if len(lst2) == 0 and attrs[key].find(" ") == -1:  # Try matching without quotation marks
+                    lst2 = re.compile('(<' + name + '[^>]*?(?:' + key + '=' + attrs[key] + '.*?>))', re.M | re.S).findall(item)
+
+                if len(lst) == 0:
+                    lst = lst2
+                    lst2 = []
+                else:
+                    test = range(len(lst))
+                    test.reverse()
+                    for i in test:  # Delete anything missing from the next list.
+                        if not lst[i] in lst2:
+                            del(lst[i])
+
+            if len(lst) == 0 and attrs == {}:
+                lst = re.compile('(<' + name + '>)', re.M | re.S).findall(item)
+                if len(lst) == 0:
+                    lst = re.compile('(<' + name + ' .*?>)', re.M | re.S).findall(item)
+
+            if isinstance(ret, str):
+                lst2 = []
+                for match in lst:
+                    attr_lst = re.compile('<' + name + '.*?' + ret + '=([\'"].[^>]*?[\'"])>', re.M | re.S).findall(match)
+                    if len(attr_lst) == 0:
+                        attr_lst = re.compile('<' + name + '.*?' + ret + '=(.[^>]*?)>', re.M | re.S).findall(match)
+                    for tmp in attr_lst:
+                        cont_char = tmp[0]
+                        if cont_char in "'\"":
+                            # Limit down to next variable.
+                            if tmp.find('=' + cont_char, tmp.find(cont_char, 1)) > -1:
+                                tmp = tmp[:tmp.find('=' + cont_char, tmp.find(cont_char, 1))]
+
+                            # Limit to the last quotation mark
+                            if tmp.rfind(cont_char, 1) > -1:
+                                tmp = tmp[1:tmp.rfind(cont_char)]
+                        else:
+                            if tmp.find(" ") > 0:
+                                tmp = tmp[:tmp.find(" ")]
+                            elif tmp.find("/") > 0:
+                                tmp = tmp[:tmp.find("/")]
+                            elif tmp.find(">") > 0:
+                                tmp = tmp[:tmp.find(">")]
+
+                        lst2.append(tmp.strip())
+                lst = lst2
+            else:
+                lst2 = []
+                for match in lst:
+                    endstr = u"</" + name
+
+                    start = item.find(match)
+                    end = item.find(endstr, start)
+                    pos = item.find("<" + name, start + 1 )
+
+                    while pos < end and pos != -1:
+                        tend = item.find(endstr, end + len(endstr))
+                        if tend != -1:
+                            end = tend
+                        pos = item.find("<" + name, pos + 1)
+
+                    if start == -1 and end == -1:
+                        temp = u""
+                    elif start > -1 and end > -1:
+                        temp = item[start + len(match):end]
+                    elif end > -1:
+                        temp = item[:end]
+                    elif start > -1:
+                        temp = item[start + len(match):]
+
+                    if ret:
+                        endstr = item[end:item.find(">", item.find(endstr)) + 1]
+                        temp = match + temp + endstr
+
+                    item = item[item.find(temp, item.find(match)) + len(temp):]
+                    lst2.append(temp)
+                lst = lst2
+            ret_lst += lst
+
+        return ret_lst
+
 
 class TvCid:
     def __init__(self, cid, name, title, strm = "", img = ""):
@@ -356,7 +461,7 @@ class baseServiceUpdater:
         try:
             self.channels = self.getChannelList()
             if len(self.channels) <= 0:
-                self.log('loadChannelList error loding channel list for service %s - aborting!' % self.serviceName)
+                self.log('loadChannelList error lodaing channel list for service %s - aborting!' % self.serviceName)
                 self.automap = list()
                 self.close()
                 return
